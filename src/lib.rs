d24d771a335cbffc401596aa6de0460b6158d3e7
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-10 16:18:07 +0000

    Introduce a Stage type

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,11 @@ pub fn run(pipeline_file: &str, writer: &mut impl Write) {
     let pipeline_string =
         fs::read_to_string(pipeline_file).unwrap();
 
-    let yaml: HashMap<String, Sequence> =
-        serde_yaml::from_str(&pipeline_string).unwrap();
+    let stages_new = parse_stages(&pipeline_string);
 
-    let stages: Vec<String> =
-        yaml
-        .get("stages")
-        .unwrap()
-        .iter()
-        .map(|value| value.as_mapping().unwrap())
+    let stages: Vec<String> = stages_new.iter()
         .map(|stage| {
-            let name =
-                stage.get("name").unwrap().as_str().unwrap();
-
-            let command =
-                stage.get("command").unwrap().as_str().unwrap();
-
-            return String::from("name: ") + name + ", command: " + command;
+            return String::from("name: ") + &stage.name + ", command: " + &stage.command;
         })
         .collect();
 
@@ -32,4 +20,31 @@ pub fn run(pipeline_file: &str, writer: &mut impl Write) {
         Ok(_) => (),
         Err(_) => ()
     }
+}
+
+struct Stage {
+    name: String,
+    command: String
+}
+
+fn parse_stages(pipeline_string: &str) -> Vec<Stage> {
+    let yaml: HashMap<String, Sequence> =
+        serde_yaml::from_str(&pipeline_string).unwrap();
+
+    let stages: Vec<Stage> =
+        yaml
+            .get("stages")
+            .unwrap()
+            .iter()
+            .map(|value| {
+                let stage = value.as_mapping().unwrap();
+
+                return Stage {
+                    name: stage.get("name").unwrap().as_str().unwrap().to_string(),
+                    command: stage.get("command").unwrap().as_str().unwrap().to_string()
+                };
+            })
+            .collect();
+
+    return stages
 }
