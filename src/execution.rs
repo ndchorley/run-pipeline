f65eedc32973271441250a26077da0edd67ce001
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-16 19:22:59 +0000

    Extract a run_stage method in Pipeline

--- src/execution.rs
+++ src/execution.rs
@@ -1,30 +1,32 @@
 use std::{io::{Result, Write}, process::{Command, Output}};
 
-use crate::{display::*, domain::Pipeline};
+use crate::{display::*, domain::*};
 
 impl Pipeline {
     pub fn run_stages(&self, writer: &mut impl Write) {
         let _: Vec<_> =
             self.stages
                 .iter()
-                .map(|stage| {
-                    display_running_message(&stage.name, writer);
-
-                    let output = Self::execute(&stage.command).unwrap();
-                    display_command_output(output.stdout, writer);
-
-                    display_finished_message(
-                        &stage.name, output.status, writer
-                    );
-
-                    if output.status.success() {
-                        Ok(())
-                    } else {
-                        Err("")
-                    }
-                })
+                .map(|stage| { Self::run_stage(stage, writer) })
                 .take_while(|result| result.is_ok())
                 .collect();
+    }
+
+    fn run_stage(
+        stage: &Stage, writer: &mut impl Write
+    )  -> core::result::Result<(), ()> {
+        display_running_message(&stage.name, writer);
+
+        let output = Self::execute(&stage.command).unwrap();
+        display_command_output(output.stdout, writer);
+
+        display_finished_message(&stage.name, output.status, writer);
+
+        if output.status.success() {
+            Ok(())
+        } else {
+            Err(())
+        }
     }
 
     fn execute(command: &String) -> Result<Output> {
