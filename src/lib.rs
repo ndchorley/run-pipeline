575f1e4de3c3ee897a2744ccf3ddbc9b75e284b5
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-16 10:47:11 +0000

    Introduce display_command_output

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::Write};
+use std::{fs, io::Write, process::Output};
 
 use domain::{Pipeline, Stage};
 use execution::execute;
@@ -22,11 +22,8 @@ pub fn run(pipeline_file: &str, writer: &mut impl Write) {
                     display_running_message(stage, writer);
 
                     let output = execute(stage).unwrap();
+                    display_command_output(output, writer);
 
-                    let command_output =
-                        String::from_utf8(output.stdout).unwrap();
-                    writeln!(writer, "{}", &command_output).unwrap();
-            
                     let finished_stage_message =
                         String::from(&stage.name) + " succeeded";
                     writeln!(writer, "{}", &finished_stage_message).unwrap();
@@ -47,4 +44,11 @@ fn display_running_message(stage: &Stage, writer: &mut impl Write) {
         String::from("Running ") + &stage.name + "...";
 
     writeln!(writer, "{}", &running_stage_message).unwrap();
+}
+
+fn display_command_output(output: Output, writer: &mut impl Write) {
+    let command_output =
+        String::from_utf8(output.stdout).unwrap();
+
+    writeln!(writer, "{}", &command_output).unwrap();
 }
