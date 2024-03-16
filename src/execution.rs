ed6131570ddd0caf6cc14406700496469515a0fc
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-16 10:01:02 +0000

    Break some long lines

--- src/execution.rs
+++ src/execution.rs
@@ -5,13 +5,17 @@ use crate::Stage;
 pub fn execute(stage: &Stage, writer: &mut impl Write) {
     let running_stage_message =
         String::from("Running ") + &stage.name + "...";
-    writeln!(writer, "{}", &running_stage_message).unwrap();
-
-    let output = Command::new(&stage.command).output().unwrap();
-
-    writeln!(writer, "{}", String::from_utf8(output.stdout).unwrap())
+    writeln!(writer, "{}", &running_stage_message)
         .unwrap();
 
-    let finished_stage_message = String::from(&stage.name) + " succeeded";
+    let output =
+        Command::new(&stage.command).output().unwrap();
+
+    let command_output =
+        String::from_utf8(output.stdout).unwrap();
+    writeln!(writer, "{}", &command_output).unwrap();
+
+    let finished_stage_message =
+        String::from(&stage.name) + " succeeded";
     writeln!(writer, "{}", &finished_stage_message).unwrap();
 }
