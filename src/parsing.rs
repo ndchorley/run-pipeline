07c7a112f96f1b75e487814b11fa350fdf735939
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-29 14:00:47 +0000

    Refactor to introduce parse_yaml

--- src/parsing.rs
+++ src/parsing.rs
@@ -5,11 +5,15 @@ use crate::Pipeline;
 use crate::Stage;
 
 pub fn parse_pipeline(pipeline_string: &str) -> Result<Pipeline, String> {
-    serde_yaml::from_str::<HashMap<String, Sequence>>(&pipeline_string)
-        .map_err(|_| { String::from("Could not parse pipeline") } )
+    parse_yaml(pipeline_string)
         .and_then(|yaml| find_stages_sequence(&yaml))
         .and_then(|stages_sequence| parse_stages(stages_sequence))
         .and_then(|stages| Ok(Pipeline { stages }))
+}
+
+fn parse_yaml(pipeline_string: &str) -> Result<HashMap<String, Sequence>, String> {
+    serde_yaml::from_str::<HashMap<String, Sequence>>(&pipeline_string)
+        .map_err(|_| { String::from("Could not parse pipeline") } )
 }
 
 fn find_stages_sequence(yaml: &HashMap<String, Vec<Value>>) -> Result<Vec<Value>, String> {
