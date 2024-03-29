7c6609ef5c9e4bf4b8363ee27736a7f8adc02237
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-03-29 11:15:16 +0000

    Refactor to introduce parse_stages

--- src/parsing.rs
+++ src/parsing.rs
@@ -8,15 +8,8 @@ pub fn parse_pipeline(pipeline_string: &str) -> Result<Pipeline, String> {
     serde_yaml::from_str::<HashMap<String, Sequence>>(&pipeline_string)
         .map_err(|_| { String::from("Could not parse pipeline") } )
         .and_then(|yaml| find_stages_sequence(&yaml))
-        .and_then(|stages_sequence| {
-            let stages =
-                stages_sequence
-                    .iter()
-                    .map(|value| parse_stage(value))
-                    .collect();
-
-            Ok(Pipeline { stages })
-        })
+        .and_then(|stages_sequence| parse_stages(stages_sequence))
+        .and_then(|stages| Ok(Pipeline { stages }))
 }
 
 fn find_stages_sequence(yaml: &HashMap<String, Vec<Value>>) -> Result<Vec<Value>, String> {
@@ -24,6 +17,16 @@ fn find_stages_sequence(yaml: &HashMap<String, Vec<Value>>) -> Result<Vec<Value>
         Some(stages_sequence) => Ok(stages_sequence.to_vec()),
         None => Err(String::from("Could not parse pipeline: missing a sequence called 'stages'"))
     }
+}
+
+fn parse_stages(stages_sequence: Vec<Value>) -> Result<Vec<Stage>, String> {
+    let stages =
+        stages_sequence
+            .iter()
+            .map(|value| parse_stage(value))
+            .collect();
+
+    Ok(stages)
 }
 
 fn parse_stage(value: &Value) -> Stage {
