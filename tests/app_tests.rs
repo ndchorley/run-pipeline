c47ef9de261af8fc6ade7f8fec047625f4aa4986
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-04-07 17:59:34 +0100

    Use assertor in app_tests.rs

--- tests/app_tests.rs
+++ tests/app_tests.rs
@@ -1,3 +1,4 @@
+use assertor::*;
 use run_pipeline::run;
 
 mod helpers;
@@ -10,17 +11,16 @@ fn it_runs_the_stages_in_the_pipeline() {
 
     run("tests/successful/pipeline.yml", &mut output);
 
-    assert_eq!(
-        as_string(output),
-
-        "Running Build...\n\
-        some build output\n\n\
-        \x1B[0;32mBuild succeeded\x1B[0m\n\n\
-        \
-        Running Deploy...\n\
-        some deploy output\n\n\
-        \x1B[0;32mDeploy succeeded\x1B[0m\n\n"
-    );
+    assert_that!(as_string(output))
+        .is_equal_to(
+            "Running Build...\n\
+            some build output\n\n\
+            \x1B[0;32mBuild succeeded\x1B[0m\n\n\
+            \
+            Running Deploy...\n\
+            some deploy output\n\n\
+            \x1B[0;32mDeploy succeeded\x1B[0m\n\n".to_string()
+        );
 }
 
 #[test]
@@ -29,13 +29,12 @@ fn it_does_not_run_subsequent_stages_after_a_failure() {
 
     run("tests/failing-build/pipeline.yml", &mut output);
 
-    assert_eq!(
-        as_string(output),
-
-        "Running Build...\n\
-        some failure\n\n\
-        \x1B[0;31mBuild failed\x1B[0m\n\n"
-    );
+    assert_that!(as_string(output))
+        .is_equal_to(
+            "Running Build...\n\
+            some failure\n\n\
+            \x1B[0;31mBuild failed\x1B[0m\n\n".to_string()
+        );
 }
 
 #[test]
@@ -44,8 +43,8 @@ fn it_complains_if_the_pipeline_cant_be_found() {
 
     run("tests/does-not-exist.yml", &mut output);
 
-    assert_eq!(
-        as_string(output),
-        "Could not find pipeline at tests/does-not-exist.yml\n"
-    )
+    assert_that!(as_string(output))
+        .is_equal_to(
+            "Could not find pipeline at tests/does-not-exist.yml\n".to_string()
+        )
 }
