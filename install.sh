457dfc20ad0bba6c8212dbed956285cda967b7bb
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-05 19:16:05 +0100

    Check whether run-pipeline exists in ~/bin before trying to delete it

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-cp -v ./target/release/run-pipeline ~/bin/run-pipeline-new &&
-    rm -v ~/bin/run-pipeline &&
-    mv -v ~/bin/run-pipeline-new ~/bin/run-pipeline
+cp -v ./target/release/run-pipeline ~/bin/run-pipeline-new
+
+if [ -e ~/bin/run-pipeline ]
+then
+    rm -v ~/bin/run-pipeline
+fi
+
+mv -v ~/bin/run-pipeline-new ~/bin/run-pipeline
